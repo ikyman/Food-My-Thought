e0c13b93d6350457959eee39e4fa0f1594422d10
Svet <70716489+ikyman@users.noreply.github.com>
2025-07-13 14:45:01 -0400

    Update React Router
    Now version 7.5.2, so It no longer has the Security Vulnerabiltiy

---
+++ src/utilities/UndoObj.ts
@@ -0,0 +1,16 @@
+
+// Note! I want for the functionalist to add fooditems to actually exist before implimenting Undo.
+
+class fooditemChanged {
+  localID: int;
+  htmlTag: string
+  formerStatus: foodItem
+
+  constructor(month: int, day: int ) {
+    this.month = month;
+    this.day = day;
+
+  }
+
+
+}
