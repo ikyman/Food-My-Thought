4058851d293fbd1c43a45d50ef51d6efea82ddc4
Svet <70716489+ikyman@users.noreply.github.com>
2025-11-21 22:25:24 -0500

    Now inherit from AbstractBaseUser.
    In an attempt to stamp out any remnants of the insolent username Field, I made my usernameless user inherit from Abstract Base User, with PermissionsMixin to allow superusers to exist.
    As PermissionsMixin only has a feild for is_superuser, and as my admin panel was hounding me about a lack of is_staff & is_admin, I added them to Usernameless User.
    I then got BTFO-ed wresting with the migration manager.
    Hey, from my perspective, makemigrations, migrate should be good enough. But no, I had to do some silly comment dance ( https://stackoverflow.com/questions/44651760/django-db-migrations-exceptions-inconsistentmigrationhistory )
    Even though I pranced in said silly waltz, I had to --run-syncdb , offing my existing admin. Sigh.
    And now? It's no longer complaining about no is staff, but again complaining about no username.
    Unfortunent, but at least I have a suspected culprit: UsernamelessCreationForm
    
    A note for the future: One of the contributers in the above stackoverflow post gives a couple of options for fixing an InconsistentMigrationHistory error without sliming my table. I can live with killing of my table every so often, though one day I will not be, and it would be best to prepare for that day.

--- FMTBackEnd/userNoUsername/models.py
+++ FMTBackEnd/userNoUsername/models.py
@@ -1,12 +1,37 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
 # Create your models here.
+class UsernamelessUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(
+            email=self.normalize_email(email),
+        )
 
-class UsernamelessUser(AbstractUser):
-    email = models.CharField( max_length = 255, unique = True)
-    url_exten = models.BigAutoField( primary_key = True )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_superuser(self, email, password):
+        user = self.create_user(email,
+            password=password,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user        
+
+class UsernamelessUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    url_exten = models.BigAutoField(primary_key=True)
+    
+    is_staff = models.BooleanField(default = False)
+    is_admin = models.BooleanField(default = False)
     
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+    
+    objects = UsernamelessUserManager()
