8208c2f2ffeb2d96a9553aa754e2d647c619fd0a
Svet <70716489+ikyman@users.noreply.github.com>
2025-11-21 22:10:44 -0500

    Register Admin.
    Good news! In the beginning, it worked! I could create a superuser, and gain admin access to my website. Neat!
    Bad News! I could create A superuser. A single superuser. If I tried to create a second user, I get "error: the username is not unique"
    Huh? I set the username field to the email, the e-mail should be unique. What's going on?

--- FMTBackEnd/FMTAPIsPublic/admin.py
+++ FMTBackEnd/FMTAPIsPublic/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import UserLarder, FoodItem
 
 # Register your models here.
+
+admin.site.register(UserLarder)
+admin.site.register(FoodItem)
--- FMTBackEnd/userNoUsername/admin.py
+++ FMTBackEnd/userNoUsername/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import UsernamelessUser
+from .forms import UsernamelessCreationForm
+
+class UsernamelessAdmin(UserAdmin):
+ add_form = UsernamelessCreationForm
+ 
+ search_fields = ["email"]
+ ordering = ["email"]
+ list_display = ["email", "url_exten"]
+ list_filter = ["email","url_exten"]
+ 
 
 # Register your models here.
-
-admin.site.register(UsernamelessUser, UserAdmin)
+admin.site.register(UsernamelessUser, UsernamelessAdmin)
