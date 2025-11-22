4a9a14c8bb7a27eff1625a3f4f0c15290237228b
Svet <70716489+ikyman@users.noreply.github.com>
2025-11-21 23:01:19 -0500

    TDB: Username finally Vanquished?
    Y'know how I said the admin was still entering in a username into the username creation form?
    Here's how do deal with that:
    1. https://docs.djangoproject.com/en/5.2/topics/auth/customizing/#custom-users-and-proxy-models : Specifically, the subsection on forms
    1. Lower-level Creation form. Does inheriting from User Creation form cause the form to expect a username?  I don't know, for that possibility has been nipped in the bud.
    2. Add an ugly list of pair tuples of maps. If I had to guess, this is what governs "What gets fed into the forms", defined for user creation.

--- FMTBackEnd/userNoUsername/admin.py
+++ FMTBackEnd/userNoUsername/admin.py
@@ -5,11 +5,23 @@ from .forms import UsernamelessCreationForm
 
 class UsernamelessAdmin(UserAdmin):
  add_form = UsernamelessCreationForm
- 
- search_fields = ["email"]
- ordering = ["email"]
  list_display = ["email", "url_exten"]
  list_filter = ["email","url_exten"]
+ fieldsets = [
+  (None, {"fields": ["email", "password"]}),
+  ("Permissions", {"fields": ["is_admin"]}),
+ ] 
+ add_fieldsets = [
+  (
+   None,
+   {
+    "classes": ["wide"],
+    "fields": ["email", "password1", "password2"],
+   },
+  ),
+ ] 
+ search_fields = ["email"]
+ ordering = ["email"]
  
 
 # Register your models here.
--- FMTBackEnd/userNoUsername/forms.py
+++ FMTBackEnd/userNoUsername/forms.py
@@ -1,8 +1,38 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm, CharField, ValidationError, PasswordInput
 from .models import UsernamelessUser
 
-class UsernamelessCreationForm(UserCreationForm):
- class Meta(UserCreationForm.Meta):
-  model = UsernamelessUser
-  fields = ('email', password1, password2)
+class UsernamelessCreationForm(ModelForm):
+   """
+   A form that creates a user, with no privileges, from the given username and
+   password.
+   """
+   error_messages = {
+       'password_mismatch': "The two password fields didn't match.",
+   }
+   password1 = CharField(label="Password",
+       widget=PasswordInput)
+   password2 = CharField(label= "Password confirmation",
+       widget=PasswordInput,
+       help_text="Enter the same password as above, for verification.")
 
+   class Meta():
+      model = UsernamelessUser
+      fields = ['email']
+      
+
+   def clean_password2(self):
+      password1 = self.cleaned_data.get("password1")
+      password2 = self.cleaned_data.get("password2")
+      if password1 and password2 and password1 != password2:
+         raise ValidationError(
+            self.error_messages['password_mismatch'],
+            code='password_mismatch',
+         )
+      return password2
+
+   def save(self, commit=True):
+      user = super(UserCreationForm, self).save(commit=False)
+      user.set_password(self.cleaned_data["password1"])
+      if commit:
+         user.save()
+      return user
