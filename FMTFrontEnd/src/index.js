82565396513e280b45dce69a7bdf03ef494dbd01
Svet <70716489+ikyman@users.noreply.github.com>
2025-12-28 16:54:52 -0500

    Ah! React OIDC! You may have Hidden, but your doom is inevitable.

--- FMTFrontEnd/src/index.js
+++ FMTFrontEnd/src/index.js
@@ -2,27 +2,17 @@ import React, {useState} from 'react';
 import { createRoot } from 'react-dom/client';
 
 import ReactDOM from 'react-dom/client';
-import { AuthProvider } from "react-oidc-context";
 import { QueryDjangoBackendContextProvider} from './context/QueryDjangoBackendContext/QueryDjangoBackendContextProvider'
 
 import WebPageNavigation from './WebPageNavigation';
 
-const cognitoAuthConfig = {
-  authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_lsIpZnYWc",
-  client_id: "47piu68hu52i75d44npusb50uk",
-  redirect_uri: "http://localhost:3000/",
-  response_type: "code",
-  scope: "email openid phone"
-};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
       <QueryDjangoBackendContextProvider>
-      <AuthProvider {...cognitoAuthConfig}>
           <WebPageNavigation />
-      </AuthProvider>
     </QueryDjangoBackendContextProvider>
   </React.StrictMode>
 );
