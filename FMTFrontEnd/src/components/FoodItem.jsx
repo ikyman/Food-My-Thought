a0eb1ede50e363df2d4a6a36bbc914b36ca01d76
Svet <70716489+ikyman@users.noreply.github.com>
2026-02-18 22:38:46 -0500

    Navigating table by <= & => Works. PROTOTYPE
    PROTOTYPE because it looks like a steaming pile.
    
    I am currently looking into useMemo; that should store the array of textareas, preventing me from having to have 2 slices of code that do the same thing.

--- FMTFrontEnd/src/components/FoodItem.jsx
+++ FMTFrontEnd/src/components/FoodItem.jsx
@@ -2,8 +2,6 @@ import React, { useState, useRef} from 'react';
 
 import { useLarderContext } from '../context/LarderContext/LarderContext'
 
-
-
 export class FoodItem{
     constructor(foodItemJson){
         this.id = foodItemJson["id"]
@@ -21,10 +19,51 @@ export function renderFoodItem(foodItem){
         backEndSave(foodItem)
     }
 
-    return (<tr className="fooditem-entry" tabIndex="0" key={foodItem.id} onBlur = {(e) => {foodItemToBackEnd(e);} }>
-                <td> <textarea value = {foodItem.name} onChange = { (e) => {foodItem.name = e.target.value;}} ></textarea></td>
-                <td><textarea>{foodItem.estimated_expiration}</textarea></td>
-                <td><textarea>{foodItem.category1}</textarea></td>
-                <td><textarea>{foodItem.category2}</textarea></td>
+    const foodItemNav = (keyInput) =>{
+        const textareaCalled = document.activeElement;
+        if (textareaCalled.tagName !== "TEXTAREA"){
+            return
+        }
+        if (keyInput.key === "ArrowUp" || keyInput.key === "ArrowDown"){
+            const rce = new CustomEvent("RowChangeEvent", 
+                {detail: {callingFoodItem: foodItem.id, direction: keyInput.key}}
+            )
+            dispatchEvent(rce);
+            return;
+        }else if (keyInput.key === "ArrowLeft" && (textareaCalled.selectionStart === 0 && textareaCalled.selectionEnd === 0)){
+            const foodItem_tr = textareaCalled.closest("tr");
+            const textareas = Array.from(foodItem_tr.querySelectorAll("textarea"));
+            console.log(textareas);
+            const currentIndex = textareas.indexOf(textareaCalled);
+            if (currentIndex != 0){
+                console.log(currentIndex);
+                let ltextArea = textareas[currentIndex - 1];
+                ltextArea.focus();
+                ltextArea.selectionStart = ltextArea.textLength;
+                ltextArea.selectionEnd = ltextArea.textLength;
+            }
+        }else if (keyInput.key === "ArrowRight" && (textareaCalled.selectionStart === textareaCalled.textLength && textareaCalled.selectionEnd === textareaCalled.textLength )){
+            const foodItem_tr = textareaCalled.closest("tr");
+            const textareas = Array.from(foodItem_tr.querySelectorAll("textarea"));
+            const currentIndex = textareas.indexOf(textareaCalled);
+            if (currentIndex !== -1 && currentIndex < textareas.length - 1){
+                let rtextArea = textareas[currentIndex + 1];
+                rtextArea.focus();
+                rtextArea.selectionStart = rtextArea.textLength;
+                rtextArea.selectionEnd = rtextArea.textLength;
+            }
+        }
+    }
+
+    return (<tr className="fooditem-entry" 
+                tabIndex="0"
+                key={foodItem.id}
+                onBlur = {(e) => {foodItemToBackEnd(e);} }
+                onKeyDown={(e) => {foodItemNav(e);} }
+                >
+                <td><textarea className = "name-textarea" value = {foodItem.name} onChange = { (e) => {foodItem.name = e.target.value;}} ></textarea></td>
+                <td><textarea className = "date-textarea">{foodItem.estimated_expiration}</textarea></td>
+                <td><textarea className = "cat1-textarea">{foodItem.category1}</textarea></td>
+                <td><textarea className = "cat2-textarea">{foodItem.category2}</textarea></td>
             </tr>)
 }
