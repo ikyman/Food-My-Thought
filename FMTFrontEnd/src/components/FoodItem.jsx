4157ec3fe7394f52303746cf7c5506f302f36eb2
Svet <70716489+ikyman@users.noreply.github.com>
2026-04-22 17:05:53 -0400

    Cleaned Arrow-nav Prototype & Moved it to FoodItem Table
    This works roughly the same as it did in commit https://github.com/ikyman/Food-My-Thought/commit/a0eb1ede50e363df2d4a6a36bbc914b36ca01d76
    
    I am better than an AI, though, so the arrow-nav logic is cleaner.
    
    Cleaner, but for how long? I will have to implement a very simular logic for the up & down keys. Furthermore, category name-changing hasn't been implemented yet; including those two textboxes throws a further wrench into an future attempt to wrap up & down & left & right navigation into one nice clean function.

--- FMTFrontEnd/src/components/FoodItem.jsx
+++ FMTFrontEnd/src/components/FoodItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef} from 'react';
+import React from 'react';
 
 import { useLarderContext } from '../context/LarderContext/LarderContext'
 
@@ -30,28 +30,6 @@ export function renderFoodItem(foodItem){
             )
             dispatchEvent(rce);
             return;
-        }else if (keyInput.key === "ArrowLeft" && (textareaCalled.selectionStart === 0 && textareaCalled.selectionEnd === 0)){
-            const foodItem_tr = textareaCalled.closest("tr");
-            const textareas = Array.from(foodItem_tr.querySelectorAll("textarea"));
-            console.log(textareas);
-            const currentIndex = textareas.indexOf(textareaCalled);
-            if (currentIndex != 0){
-                console.log(currentIndex);
-                let ltextArea = textareas[currentIndex - 1];
-                ltextArea.focus();
-                ltextArea.selectionStart = ltextArea.textLength;
-                ltextArea.selectionEnd = ltextArea.textLength;
-            }
-        }else if (keyInput.key === "ArrowRight" && (textareaCalled.selectionStart === textareaCalled.textLength && textareaCalled.selectionEnd === textareaCalled.textLength )){
-            const foodItem_tr = textareaCalled.closest("tr");
-            const textareas = Array.from(foodItem_tr.querySelectorAll("textarea"));
-            const currentIndex = textareas.indexOf(textareaCalled);
-            if (currentIndex !== -1 && currentIndex < textareas.length - 1){
-                let rtextArea = textareas[currentIndex + 1];
-                rtextArea.focus();
-                rtextArea.selectionStart = rtextArea.textLength;
-                rtextArea.selectionEnd = rtextArea.textLength;
-            }
         }
     }
 
@@ -59,7 +37,6 @@ export function renderFoodItem(foodItem){
                 tabIndex="0"
                 key={foodItem.id}
                 onBlur = {(e) => {foodItemToBackEnd(e);} }
-                onKeyDown={(e) => {foodItemNav(e);} }
                 >
                 <td><textarea className = "name-textarea" value = {foodItem.name} onChange = { (e) => {foodItem.name = e.target.value;}} ></textarea></td>
                 <td><textarea className = "date-textarea">{foodItem.estimated_expiration}</textarea></td>
--- FMTFrontEnd/src/components/FooditemTable.jsx
+++ FMTFrontEnd/src/components/FooditemTable.jsx
@@ -19,10 +19,58 @@ export default function FooditemTable({loadedCategoryNames, viewAsOwner}) {
         larderContext.addFoodItem()
     }
 
+    const onFoodTableArrowNav = (keyInput) => {
+        if (!keyInput.key.startsWith("Arrow")) {
+            return;
+        }
+        const textareaCalled = document.activeElement;
+        const currentRow = keyInput.target.closest("tr");
+        const ctrlKeyed = keyInput.ctrlKey;
+
+        if (!textareaCalled || textareaCalled.tagName !== "TEXTAREA") {
+            return;
+        }
+
+        if (keyInput.key === "ArrowUp" || keyInput.key === "ArrowDown") {
+            console.log("Current fooditem-entry row:", currentRow);
+
+            return;
+        }
+
+        if (keyInput.key === "ArrowLeft" || keyInput.key === "ArrowRight") {
+            const atTextAreaStart = (textareaCalled.selectionStart === 0 && textareaCalled.selectionEnd === 0);
+            const atTextAreaEnd = (textareaCalled.selectionStart === textareaCalled.textLength && textareaCalled.selectionEnd === textareaCalled.textLength);
+
+            const foodItemTextareas = Array.from(currentRow.querySelectorAll("textarea"));
+            const currentIndex = foodItemTextareas.indexOf(textareaCalled);
+
+            let nextIndex = undefined;
+            if (keyInput.key === "ArrowLeft" && atTextAreaStart){
+                nextIndex = Math.max(0, currentIndex-1);
+                if (ctrlKeyed){
+                    nextIndex = 0;
+                }
+            }
+            if (keyInput.key === "ArrowRight" && atTextAreaEnd){
+                nextIndex = Math.min(foodItemTextareas.length - 1, currentIndex+1);
+                if (ctrlKeyed){
+                    nextIndex = foodItemTextareas.length - 1;
+                }
+            }
+            if (nextIndex === undefined){
+                return;
+            }
+            const selectedTextArea = foodItemTextareas[nextIndex];
+            selectedTextArea.focus();
+            keyInput.preventDefault();
+
+            console.log("Current DOM textbox:", textareaCalled);
+        }
+    }
+
     return (
         <div id="fooditem-list">
-
-            <table id="fooditem-table">
+            <table id="fooditem-table" onKeyDown={onFoodTableArrowNav}>
                 <thead>
                     <tr>
                         <th>Food Name</th>
