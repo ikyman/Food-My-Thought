f6afd282bb9e9055f8ea44447c379555d0a3d83d
Svet <70716489+ikyman@users.noreply.github.com>
2026-06-25 22:59:44 -0400

    logInRequiredTest & deleteTestUser
    This test tests that an endpoint requires the user to be logged in before editing said endpoint.
    Useful for reducing the number of assertEqual(401)s I have to write!
    
    Not so useful for the Recipe Item. The recipe Item has two different edit permissions, depending on who's editing the recipe. Perhaps split the recipe's POST into 2?
    
    There also was the 201 Created, 200 edit testcase. Currently half-done because I doubted the lines-of-code savings was worth the awkward "creation201200test" name.

--- FMTBackEnd/FMTAPIsPublic/unitTests/fmtViewTestcase.py
+++ FMTBackEnd/FMTAPIsPublic/unitTests/fmtViewTestcase.py
@@ -25,19 +25,53 @@ class FmtViewTestcase(TestCase):
     FmtViewTestcase.user_count += 1
 
     return created_user
-
+   
  def createTestLarder(self, user_description = None, discontinuation = None  ):
     if user_description == None:
         user_description = "Test Larder" + str(FmtViewTestcase.user_count)
     created_user = self.createTestUserNoLarder()
 
+    if (discontinuation == None):
+      discontinuation = default_larder_expiration()
+
     created_larder = UserLarder(url_exten = created_user,
                                 user_description = user_description,
-                                discontinue_date = default_larder_expiration(),
+                                discontinue_date = discontinuation,
                                 )
     created_larder.save()
 
     return (created_user, created_larder)
+
+ def deleteTestUser(self, url_exten):
+   self.testUsersPasswords.pop(url_exten)
+
+   user = User.objects.get(url_exten = url_exten)
+
+   user.delete() # ON DELETE CASCADE: Deleting the user is all that matters
+ 
+ def jsonTestItem(self, **kwargs):
+   raise "Calling Abstract function outside of sub-class overloading!"
+
+ def creation201200test(self, url_slug, added_item_name):
+   addedJSONItem = self.jsonTestItem()
+
+   response = self.client.post(url_slug, addedFoodItem) 
+   self.assertEqual(response.status_code, 201, "Should return 201 when creating a new " + added_item_name)
+
+   pass
+  
+ def logInRequiredTest(self, clientHTTPMethod, url_slug, httpRequestData = None):
+  # While logged out
+  self.client.logout()
+  response = clientHTTPMethod(url_slug, httpRequestData)
+  self.assertEqual(response.status_code, 401 , "Cannot Use API Endpoint" + url_slug + "When not logged in")
+
+  # While logged in as different user
+  unauthUser = self.createTestUserNoLarder()
+  self.loginUser(unauthUser)
+  
+  response = clientHTTPMethod(url_slug, httpRequestData)
+  self.assertEqual(response.status_code, 401 , "API Endpoint" + url_slug + "unavailable to foreign users")
     
  def setUp(self):
   self.client = Client()
