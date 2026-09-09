88d3caaa94c1a46e01e6424e658c9fe66b78147e
Svet <70716489+ikyman@users.noreply.github.com>
2026-09-08 23:55:55 -0400

    JsonFoodItem to reduce Repeated code for Basic FoodItem.

--- FMTBackEnd/FMTAPIsPublic/unitTests/testFoodItems.py
+++ FMTBackEnd/FMTAPIsPublic/unitTests/testFoodItems.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
 from django.contrib.auth import get_user_model, login
-from FMTAPIsPublic import views
+from FMTAPIsPublic import fooditemViews
 from FMTAPIsPublic.models import UserLarder, FoodItem
 from datetime import date, timedelta
 from .fmtViewTestcase import FmtViewTestcase
@@ -13,28 +13,30 @@ class FoodItemTestCase(FmtViewTestcase):
     self.noLarderUser = self.createTestUserNoLarder()
     self.testUrl_Exten = self.testUser.url_exten
 
-    self.loginUser(self.testUser)  
+    self.loginUser(self.testUser)
+
+ def jsonTestItem(self, **kwargs):
+   testFoodItem = dict()
+   testFoodItem["id"] = ""
+   testFoodItem["larder"] = self.testUrl_Exten
+   testFoodItem["name"] = "testFood"
+
+   return testFoodItem
  
  def testAddFoodItem(self):
   no_food = FoodItem.objects.filter()
   self.assertEqual(len(no_food), 0, "Initially: No Food Items")
-  addedFoodItem = dict()
-  addedFoodItem["id"] = ""
-  addedFoodItem["larder"] = self.testUrl_Exten
-  addedFoodItem["name"] = "testFood"
+  addedFoodItem = self.jsonTestItem()
   
   response = self.client.post("/fooditems/", addedFoodItem) 
   self.assertEqual(response.status_code, 201, "Should return 201 when creating a new FoodItem")
     
-  nowFood = FoodItem.objects.filter(name = "testFood")
+  nowFood = FoodItem.objects.filter(name = addedFoodItem["name"])
   
   self.assertEqual(len(nowFood), 1, "Food Should be added")
   
  def testEditFoodItem(self):
-  addedFoodItem = dict()
-  addedFoodItem["id"] = ""  
-  addedFoodItem["larder"] = self.testUrl_Exten
-  addedFoodItem["name"] = "testFood"
+  addedFoodItem = self.jsonTestItem()
   response = self.client.post("/fooditems/", addedFoodItem) 
   self.assertEqual(response.status_code, 201, "Can't Edit a FoodItem if there's no FoodItem to begin with!")
  
@@ -43,7 +45,6 @@ class FoodItemTestCase(FmtViewTestcase):
   
   response = self.client.post("/fooditems/", editedResponseFood) 
   self.assertEqual(response.status_code, 200, "Response code should be 200")
-  
   
   editedFood = FoodItem.objects.filter(name = "differentName")
   
@@ -56,31 +57,38 @@ class FoodItemTestCase(FmtViewTestcase):
  def testAddFoodItemToWrongLarder(self):
   no_food = FoodItem.objects.filter()
   self.assertEqual(len(no_food), 0, "Initially: No Food Items")
-  addedFoodItem = dict()
-  addedFoodItem["name"] = "testFood"
+  addedFoodItem = self.jsonTestItem()
+  del addedFoodItem["larder"]
   response = self.client.post("/fooditems/", addedFoodItem) 
-  self.assertEqual(response.status_code, 401 , "Need a Larder to add any food")
+  self.assertEqual(response.status_code, 401 , "Posting fooditems requires a Larder ID to validate user permissions")
   
-  self.client.logout()
-  self.client.login(email="other@unit.test", password="testPassword")
   addedFoodItem["larder"] = self.testUrl_Exten
-  
-  response = self.client.post("/fooditems/", addedFoodItem) 
-  self.assertEqual(response.status_code, 401 , "Can't add food to a larder you don't own")
+  self.logInRequiredTest(self.client.post, "/fooditems/", addedFoodItem)
   
  def testGetFoodViaLarderLoading(self):
-  addedFoodItem = dict()
-  addedFoodItem["id"] = ""  
-  addedFoodItem["larder"] = self.testUrl_Exten
-  addedFoodItem["name"] = "testFood"
+  addedFoodItem = self.jsonTestItem()
   response = self.client.post("/fooditems/", addedFoodItem) 
   self.assertEqual(response.status_code, 201, "Can't Load a FoodItem if there's no FoodItem to begin with!")  
   
-  response = self.client.get(f"/larder/{self.testUrl_Exten}/")
+  response = self.client.get(f"/larders/url-extension/{self.testUrl_Exten}/")
   self.assertEqual(response.status_code, 200, "Should return 200 when user accesses their own existing larder")
   response_data = response.json()
   
   self.assertEqual(len(response_data["fooditems"]), 1, "We added only 1 fooditem.")
   self.assertEqual(response_data["fooditems"][0]["name"],  "testFood", "We added the 'testFood' Fooditem.")
   
+ def testDeleteFoodItemWrongLarder(self):
+  deletable_fooditem = self.jsonTestItem()
+  response = self.client.post("/fooditems/", deletable_fooditem) 
+
+  deletable_fooditem_id = response.json()["id"]
+  self.logInRequiredTest(self.client.delete, "/fooditems/delete/" + str(deletable_fooditem_id))
+
+ def testDeleteNonexistantFoodItem(self):
+  self.assertEqual(False,  True, "Not Yet Implemented!.")
+
+ def testDelete(self):
+  self.assertEqual(False,  True, "Not Yet Implemented!.")
+
+
   
